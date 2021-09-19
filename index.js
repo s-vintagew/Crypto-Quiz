4819f0b1360b7883cb74cb387c99109512fcd3d9
Souhardya Sarkar <souhardyasarkar20@gnu.ac.in>
2021-09-19 15:58:58 +0530

    done copying

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var x
+
 function m() {
     var items = document.getElementsByClassName('m');
     for (var i = 0; i < items.length; i++) {
@@ -5,10 +7,8 @@ function m() {
     }
 }
 
-function mclick() {
-    document.getElementsByClassName('m').style.background = "yellow";
-}
-var x
+
+
 
 function y(x) {
     var items = document.getElementsByClassName('y');
@@ -16,16 +16,9 @@ function y(x) {
 
     for (var i = 0; i < items.length; i++) {
         alert(item);
-        items[i].innerHTML = item;
+        items[i].value = item;
     }
     for (var i = 0; i < items.length; i++) {
         items[i].style.background = "none";
-    }
-}
-
-function yclick() {
-    var items = document.getElementsByClassName('y');
-    for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
     }
 }
---
+++ indexClick.js
@@ -0,0 +1,13 @@
+function mclick() {
+    var items = document.getElementsByClassName('m');
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "yellow";
+    }
+}
+
+function yclick() {
+    var items = document.getElementsByClassName('y');
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "yellow";
+    }
+}
