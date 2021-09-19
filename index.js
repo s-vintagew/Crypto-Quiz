eaa18eaa8161b5553bf89ebe198ea77a293bea88
Bhavin <bhavinamesara20@gnu.ac.in>
2021-09-19 16:10:38 +0530

    index

--- index.js
+++ index.js
@@ -1,21 +1,334 @@
 var x
 
-function m() {
-    var items = document.getElementsByClassName('m');
+function a(x) {
+    var items = document.getElementsByClassName('a');
+    var item = items[x - 1].value;
+
     for (var i = 0; i < items.length; i++) {
-        items[i].innerHTML = "m";
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
     }
 }
 
+function b(x) {
+    var items = document.getElementsByClassName('b');
+    var item = items[x - 1].value;
 
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
 
+function c(x) {
+    var items = document.getElementsByClassName('c');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function d(x) {
+    var items = document.getElementsByClassName('d');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function e(x) {
+    var items = document.getElementsByClassName('e');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function f(x) {
+    var items = document.getElementsByClassName('f');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function g(x) {
+    var items = document.getElementsByClassName('g');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function h(x) {
+    var items = document.getElementsByClassName('h');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function h(x) {
+    var items = document.getElementsByClassName('h');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function i(x) {
+    var items = document.getElementsByClassName('i');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function j(x) {
+    var items = document.getElementsByClassName('j');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function k(x) {
+    var items = document.getElementsByClassName('k');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function l(x) {
+    var items = document.getElementsByClassName('l');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function m(x) {
+    var items = document.getElementsByClassName('m');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function n(x) {
+    var items = document.getElementsByClassName('n');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function o(x) {
+    var items = document.getElementsByClassName('o');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function p(x) {
+    var items = document.getElementsByClassName('p');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function q(x) {
+    var items = document.getElementsByClassName('q');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function r(x) {
+    var items = document.getElementsByClassName('r');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function s(x) {
+    var items = document.getElementsByClassName('s');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function t(x) {
+    var items = document.getElementsByClassName('t');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function u(x) {
+    var items = document.getElementsByClassName('u');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function v(x) {
+    var items = document.getElementsByClassName('v');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function w(x) {
+    var items = document.getElementsByClassName('w');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function w(x) {
+    var items = document.getElementsByClassName('w');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function x(x) {
+    var items = document.getElementsByClassName('x');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
 
 function y(x) {
     var items = document.getElementsByClassName('y');
     var item = items[x - 1].value;
 
     for (var i = 0; i < items.length; i++) {
-        alert(item);
+        items[i].value = item;
+    }
+    for (var i = 0; i < items.length; i++) {
+        items[i].style.background = "none";
+    }
+}
+
+function z(x) {
+    var items = document.getElementsByClassName('z');
+    var item = items[x - 1].value;
+
+    for (var i = 0; i < items.length; i++) {
         items[i].value = item;
     }
     for (var i = 0; i < items.length; i++) {
