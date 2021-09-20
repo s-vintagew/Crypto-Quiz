56a4eb827952e84cbc9312274d6f072a3ea4a8da
Souhardya Sarkar <souhardyasarkar20@gnu.ac.in>
2021-09-20 20:02:36 +0530

    push

--- JS/indexClick.js
+++ JS/indexClick.js
@@ -1,168 +1,168 @@
 function aclick() {
     var items = document.getElementsByClassName('a');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function bclick() {
     var items = document.getElementsByClassName('b');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function cclick() {
     var items = document.getElementsByClassName('c');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function dclick() {
     var items = document.getElementsByClassName('d');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function eclick() {
     var items = document.getElementsByClassName('e');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function fclick() {
     var items = document.getElementsByClassName('f');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function gclick() {
     var items = document.getElementsByClassName('g');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function hclick() {
     var items = document.getElementsByClassName('h');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function iclick() {
     var items = document.getElementsByClassName('i');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function jclick() {
     var items = document.getElementsByClassName('j');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function kclick() {
     var items = document.getElementsByClassName('k');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function lclick() {
     var items = document.getElementsByClassName('l');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function mclick() {
     var items = document.getElementsByClassName('m');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function nclick() {
     var items = document.getElementsByClassName('n');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function oclick() {
     var items = document.getElementsByClassName('o');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function pclick() {
     var items = document.getElementsByClassName('p');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function qclick() {
     var items = document.getElementsByClassName('q');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function rclick() {
     var items = document.getElementsByClassName('r');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function sclick() {
     var items = document.getElementsByClassName('s');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function tclick() {
     var items = document.getElementsByClassName('t');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function uclick() {
     var items = document.getElementsByClassName('u');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function vclick() {
     var items = document.getElementsByClassName('v');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function wclick() {
     var items = document.getElementsByClassName('w');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
 function xclick() {
     var items = document.getElementsByClassName('x');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
 
@@ -176,6 +176,6 @@ function yclick() {
 function zclick() {
     var items = document.getElementsByClassName('z');
     for (var i = 0; i < items.length; i++) {
-        items[i].style.background = "yellow";
+        items[i].style.background = "rgba(255, 208, 0, 0.2)";
     }
 }
