a6aa57f10aa3ac651b6dbb6876b8c0dd70f45641
Bhavin <bhavinamesara20@gnu.ac.in>
2021-09-23 22:15:37 +0530

    lvl3

--- JS/index.js
+++ JS/index.js
@@ -366,6 +366,26 @@ function checklvl2() {
     }
 }
 
+function checklvl3() {
+    var items = document.getElementsByTagName("input");
+    var str = "ifueaywtoehninieouhetoselit";
+    var y = "";
+    for (var i = 0; i < items.length; i++)
+        y += items[i].value;
+
+    if (y.toUpperCase() === str.toUpperCase()) {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").innerHTML = "Completed level 3";
+        document.getElementById("result").style.color = "#00ff00";
+        return true;
+    } else {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").innerHTML = "Letters Misplaced, Try Again";
+        document.getElementById("result").style.color = "Red";
+        return false;
+    }
+}
+
 function checklvl15() {
     var items = document.getElementsByTagName("input");
     var str = "rmmbrwhiodooarwhrionvravosidowinvrbaon";
