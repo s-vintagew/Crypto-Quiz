d9f24a06a8338898e19ba4e9d55b21b0adc91486
Bhavin <bhavinamesara20@gnu.ac.in>
2021-09-23 23:40:30 +0530

    lvl5

--- JS/check.js
+++ JS/check.js
@@ -78,6 +78,26 @@ function checklvl4() {
     }
 }
 
+function checklvl5() {
+    var items = document.getElementsByTagName("input");
+    var str = "godidsgodboksadasleeoscinctisistheidaif";
+    var y = "";
+    for (var i = 0; i < items.length; i++)
+        y += items[i].value;
+
+    if (y.toUpperCase() === str.toUpperCase()) {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").innerHTML = "Completed level 5";
+        document.getElementById("result").style.color = "#00ff00";
+        return true;
+    } else {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").innerHTML = "Letters Misplaced, Try Again";
+        document.getElementById("result").style.color = "Red";
+        return false;
+    }
+}
+
 function checklvl15() {
     var items = document.getElementsByTagName("input");
     var str = "rmmbrwhiodooarwhrionvravosidowinvrbaon";
