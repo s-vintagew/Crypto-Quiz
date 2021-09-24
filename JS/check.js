2ffb96a32e3cb51061708467136fc7f445d3a5fa
Bhavin <bhavinamesara20@gnu.ac.in>
2021-09-24 21:34:55 +0530

    lvl8

--- JS/check.js
+++ JS/check.js
@@ -127,7 +127,27 @@ function checklvl7() {
 
     if (y.toUpperCase() === str.toUpperCase()) {
         document.getElementById("result").style.display = "block";
-        document.getElementById("result").innerHTML = "Completed level 6";
+        document.getElementById("result").innerHTML = "Completed level 7";
+        document.getElementById("result").style.color = "#00ff00";
+        return true;
+    } else {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").innerHTML = "Letters Misplaced, Try Again";
+        document.getElementById("result").style.color = "Red";
+        return false;
+    }
+}
+
+function checklvl8() {
+    var items = document.getElementsByTagName("input");
+    var str = "ifyuivengenughyoumkemitkebutyoueanfomyoubeettepeon";
+    var y = "";
+    for (var i = 0; i < items.length; i++)
+        y += items[i].value;
+
+    if (y.toUpperCase() === str.toUpperCase()) {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").innerHTML = "Completed level 8";
         document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
