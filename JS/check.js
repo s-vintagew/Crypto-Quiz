157def6c24068d5eb338994906016eaaf78e6e99
Bhavin <bhavinamesara20@gnu.ac.in>
2021-09-24 22:28:25 +0530

    lvl9

--- JS/check.js
+++ JS/check.js
@@ -158,6 +158,26 @@ function checklvl8() {
     }
 }
 
+function checklvl9() {
+    var items = document.getElementsByTagName("input");
+    var str = "eeofhaineyoueeinooundineeingoreuineveopinhecapaciyoenoyess";
+    var y = "";
+    for (var i = 0; i < items.length; i++)
+        y += items[i].value;
+
+    if (y.toUpperCase() === str.toUpperCase()) {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").innerHTML = "Completed level 9";
+        document.getElementById("result").style.color = "#00ff00";
+        return true;
+    } else {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("result").innerHTML = "Letters Misplaced, Try Again";
+        document.getElementById("result").style.color = "Red";
+        return false;
+    }
+}
+
 function checklvl15() {
     var items = document.getElementsByTagName("input");
     var str = "rmmbrwhiodooarwhrionvravosidowinvrbaon";
