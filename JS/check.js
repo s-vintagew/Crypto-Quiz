f94843a42becd61ee0b164f3547c4322954e962b
Souhardya Sarkar <souhardyasarkar20@gnu.ac.in>
2021-09-27 20:26:43 +0530

    Update check.js

--- JS/check.js
+++ JS/check.js
@@ -9,8 +9,8 @@ function checklvl1() {
 		document.getElementById("frm").style.display="none";
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"Welcome to the real world. It sucks. You’re gonna love it!” - Monica, F.R.I.E.N.D.S.\"";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+        document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
 		document.getElementById("addr").style.display = "block";
         return true;
     } else {
@@ -32,8 +32,8 @@ function checklvl2() {
 		document.getElementById("frm").style.display="none";
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"That’s what I do, I drink and I know things.” – Tyrion Lannister, G.O.T.\"";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+       	document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
 		document.getElementById("addr").style.display = "block";
         return true;
     } else {
@@ -56,8 +56,8 @@ function checklvl3() {
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"If you really want something in life, you have to steal it. – Berlin, Money Heist\"";
 		document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+        document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -79,8 +79,8 @@ function checklvl4() {
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"Life’s tragedy is that we get old too soon and wise too late. — Benjamin Franklin\"";
 		document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+       	document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -102,8 +102,8 @@ function checklvl5() {
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"Good friends, good books, and a sleepy conscience: this is the ideal life. — Mark Twain\"";
 		document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+       	document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";*/
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -125,8 +125,8 @@ function checklvl6() {
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"Sometimes I’ll start a sentence and I don’t even know where it’s going. I hope I find it along the way. – Michael Scott, The Office\"";
 		document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+       	document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";*/
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -148,8 +148,8 @@ function checklvl7() {
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"If you can do what you do best and be happy, you’re further along in life than most people. — Leonardo DiCaprio\"";
 		document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+       	document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";*/
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -171,8 +171,8 @@ function checklvl8() {
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"If you live long enough, you’ll make mistakes. But if you learn from them, you’ll be a better person. — Bill Clinton\"";
 		document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+        document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";*/
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -194,8 +194,8 @@ function checklvl9() {
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"The secret of happiness, you see is not found in seeking more, but in developing the capacity to enjoy less. - Socrates\"";
 		document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+        document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -217,8 +217,8 @@ function checklvl10() {
 		document.getElementById("quote").style.display="block";
         document.getElementById("quote").innerHTML ="\"I believe every human has a finite number of heartbeats. I don’t intend to waste any of mine. — Neil Armstrong\"";
 		document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+        document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -240,8 +240,8 @@ function checklvl11() {
         document.getElementById("quote").style.display = "block";
         document.getElementById("quote").innerHTML = "\"Just disconnect. Once in a day sometime, sit silently and from all connections disconnect yourself.\"";
         document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+        document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -263,8 +263,8 @@ function checklvl12() {
         document.getElementById("quote").style.display = "block";
         document.getElementById("quote").innerHTML = "\"When we strive to become better than we are, everything around us becomes better too. — Paulo Coelho.\"";
         document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+        document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -286,8 +286,8 @@ function checklvl13() {
         document.getElementById("quote").style.display = "block";
         document.getElementById("quote").innerHTML = "We all need that someone who gets you like no one else<br>Right when you need it the most<br>We all need a soul to rely on, a shoulder to cry on<br>A friend through the highs and the lows<br>I'm not gonna make it alone";
         document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+        document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -309,8 +309,8 @@ function checklvl14() {
         document.getElementById("quote").style.display = "block";
         document.getElementById("quote").innerHTML = "I do the same thing I told you that I never would<br>I told you I'd change, even when I knew I never could<br>I know that I can’t find nobody else as good as you<br>I need you to stay, need you to stay, hey...";
         document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+        document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
         document.getElementById("result").style.display = "block";
@@ -332,7 +332,8 @@ function checklvl15() {
         document.getElementById("quote").style.display = "block";
         document.getElementById("quote").innerHTML = "Remember when I told you;<br>“No matter where I go;<br>I’ll leave your side;<br>You will never be alone”;<br>Even when we go through changes;<br>Even when we’re old;<br>Remember that I told you;<br>I’ll find my way back home<br>...";
         document.getElementById("addr").style.display = "block";
-        document.getElementById("result").style.color = "#00ff00";
+		document.getElementById("result").style.display = "none";
+        //document.getElementById("result").style.color = "#00ff00";
         return true;
     } else {
         document.getElementById("result").style.display = "block";
